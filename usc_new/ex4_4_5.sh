5595d2fe24d70d2c8fed16afa6958307f21c205a
arismarkogi <aris.markogi@gmail.com>
2024-04-28 10:02:26 +0300

    ex4_4_5.sh fixed

--- usc_new/ex4_4_5.sh
+++ usc_new/ex4_4_5.sh
@@ -4,26 +4,32 @@ source ./path.sh
 echo -e "Align the monophone model\n"
 steps/align_si.sh data/train data/lang_test_bg exp/mono exp/mono_aligned
 
+echo -e "Train triphone model"
+steps/train_deltas.sh 2000 10000 data/train data/lang exp/mono_aligned exp/tri
+
 echo -e "\n---------------------------------------------------------------------------\n"
 
 
-echo -e "\nCreate HCLG for unigram, aligned\n"
-./utils/mkgraph.sh data/lang_test_ug exp/mono_aligned exp/mono_aligned/graph_unigram
 
-echo -e "\nCreate HCLG for bigram, aligned\n"
-./utils/mkgraph.sh data/lang_test_bg exp/mono_aligned exp/mono_aligned/graph_bigram
+
+
+echo -e "\nCreate HCLG for triphone unigram, aligned\n"
+./utils/mkgraph.sh data/lang_test_ug exp/tri exp/tri/graph_unigram
+
+echo -e "\nCreate HCLG for tirphone bigram, aligned\n"
+./utils/mkgraph.sh data/lang_test_bg exp/tri exp/tri/graph_bigram
 
 echo -e "\n---------------------------------------------------------------------------\n"
 
 
 echo -e "\nDecode unigram with viterbi at data/dev, aligned\n"
-./steps/decode.sh exp/mono_aligned/graph_unigram data/dev exp/mono_aligned/decode_dev_unigram
+./steps/decode.sh exp/tri/graph_unigram data/dev exp/tri/decode_dev_unigram
 
 echo -e "\nDecode unigram with viterbi at data/test, aligned"
-./steps/decode.sh exp/mono_aligned/graph_unigram data/test exp/mono_aligned/decode_test_unigram
+./steps/decode.sh exp/tri/graph_unigram data/test exp/tri/decode_test_unigram
 
 echo -e "\nDecode bigram with viterbi at data/dev, aligned"
-./steps/decode.sh exp/mono_aligned/graph_bigram data/dev exp/mono_aligned/decode_dev_bigram
+./steps/decode.sh exp/tri/graph_bigram data/dev exp/tri/decode_dev_bigram
 
 echo -e "\nDecode bigram with viterbi at data/test, aligned"
-./steps/decode.sh exp/mono_aligned/graph_bigram data/test exp/mono_aligned/decode_test_bigram
+./steps/decode.sh exp/tri/graph_bigram data/test exp/tri/decode_test_bigram
